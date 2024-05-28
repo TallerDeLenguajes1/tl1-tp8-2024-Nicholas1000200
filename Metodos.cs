679de968bd6cf513effc1e1350a256efaa23f4b7
Nicholas1000200 <nico-laz@hotmail.com>
2024-05-28 16:21:25 -0300

    Primer Commit

---
+++ Metodos.cs
@@ -0,0 +1,22 @@
+namespace EspacioTareas
+{
+    public class Tarea
+    {
+        private int tareaID { get => TareaID; set => TareaID = value;}
+        private string descripcion { get => Descripcion; set => Descripcion = value;}
+        private int duracion { get => Duracion; set => Duracion = value; }
+        public int TareaID; 
+        public string Descripcion; 
+        public int Duracion; 
+
+        
+
+        public Tarea(int duracion, string descripcion, int tareaID)
+        {
+            Duracion = duracion;
+            Descripcion = descripcion;
+            TareaID = tareaID;
+        }
+
+    };
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using EspacioTareas;
+string parseUsage;
+int cantTareas;
+bool control;
+Console.WriteLine("Ingrese la cantidad de tareas a realizar");
+parseUsage = Console.ReadLine();
+control = int.TryParse(parseUsage, out cantTareas);
+if (control)
+{
+    Tarea[] tareas = new Tarea[cantTareas];
+    for (int i = 0; i < cantTareas; i++)
+    {
+        string Descripcion = "hc";
+        int TareaID = 4545563;
+        int Duracion = 60;
+        tareas[i] = new Tarea(Duracion,Descripcion,TareaID);
+    }
+foreach (Tarea tarea in tareas)
+{
+    Console.WriteLine($"{tarea.Descripcion}");
+    Console.WriteLine($"{tarea.TareaID}");
+    Console.WriteLine($"{tarea.Duracion}");
+}
+}else{
+    Console.WriteLine("El numero que ingreso no es valido");
+}
